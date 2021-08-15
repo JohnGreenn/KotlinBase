cdc138cae1977b657ebec8153e1e44b1a59bb3b3
geblog <892538233@qq.com>
2021-08-15 16:59:41 +0800

    navigation问题解决

--- app/src/main/java/com/hpkj/kotlinbase/main/MainActivity.kt
+++ app/src/main/java/com/hpkj/kotlinbase/main/MainActivity.kt
@@ -2,22 +2,34 @@ package com.hpkj.kotlinbase.main
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import androidx.activity.viewModels
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hjq.toast.ToastUtils
 import com.hpkj.core.view.base.BaseActivity
 import com.hpkj.kotlinbase.R
 import com.hpkj.kotlinbase.databinding.ActivityMainBinding
+import com.hpkj.kotlinbase.utils.LogUtil
 import kotlin.system.exitProcess
 
-class MainActivity : BaseActivity() {
+class MainActivity :BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModels<MainViewModel>()
+    private lateinit var navController: NavController
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val rootView: View = binding.root
+//        setContentView(rootView)
+//        initBottom()
+//
+//    }
 
     override fun getLayoutView(): View {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,12 +37,25 @@ class MainActivity : BaseActivity() {
     }
 
     override fun initView() {
-        binding.homeView?.init(supportFragmentManager, viewModel)
+        //binding.homeView?.init(supportFragmentManager, viewModel)
+        initBottom()
     }
 
+    private fun initBottom() {
 
-    override fun initData() {
+        // (1)初始化 navController,
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        navController = navHostFragment.navController
 
+        // (2)关联 “底部导航栏”与“navController”, 导航路由见 app/src/main/res/navigation/nav_graph.xml
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        LogUtil.e(TAG, "onSupportNavigateUp: ")
+        return navController.navigateUp()
     }
 
     override fun onBackPressed() {
